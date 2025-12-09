fac42679514f0a34270a7e649ce177addfb5c7a0
Ananya Singla <imananya07@gmail.com>
2025-12-09 19:43:52 +0530

    Changes to dataset

--- Score.py
+++ Score.py
@@ -3,27 +3,26 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 
-data= {
-    'Hours_Studied': [1,2,3,4,5,6,7,8,9,10],
-    'Score': [12,25,32,40,50,55,65,72,80,90]
-}
-df= pd.DataFrame(data)
+df = pd.read_csv("Student_Marks.csv")
 
-x= df[['Hours_Studied']]
-y= df[['Score']]
+print("First 5 records:")
+print(df.head())
 
-x_train, x_test, y_train, y_test= train_test_split(x,y,test_size=0.2, random_state=42)
+x = df[['time_study']]
+y = df[['Marks']]
 
-model= LinearRegression()
-model.fit(x_train,y_train)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-y_pred= model.predict(x_test)
+model = LinearRegression()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
 print("Predictions:", y_pred)
-mse= mean_squared_error(y_test, y_pred)
-print("Mean Squared Error:", mse)
 
-new_data= pd.DataFrame({'Hours_Studied': [10]})
-predicted_score= model.predict(new_data)
-print(f'Predicted Score for 10 hours studied: {predicted_score[0][0]:.2f}')
+mse = mean_squared_error(y_test, y_pred)
+print("MSE:", mse)
+
+new_data = pd.DataFrame({'time_study': [10]})
+pred = model.predict(new_data)
+print(f"Predicted Marks for studying 10 hours: {pred[0][0]:.2f}")
